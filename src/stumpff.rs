c69ef4c4ba6978dfaf3cd9b0b3e8efd58da0699a
Mihir Bhagat <mihirbhagat15@gmail.com>
2026-03-19 12:38:28 -0400

    Update stumpff.rs

--- src/stumpff.rs
+++ src/stumpff.rs
@@ -1,19 +1,19 @@
 #[allow(unused)]
-pub fn stumpff_c(z: f64) -> f64 {
-    match z {
-        z if z > 0. => (1. - z.sqrt().cos()) / z,
-        z if z == 0. => 1. / 2.,
-        z if z < 0. => (1. - -z.sqrt().cosh()) / z,
+pub fn stumpff_c(x: f64) -> f64 {
+    match x {
+        x if x > 0. => (1. - x.sqrt().cos()) / x,
+        x if x == 0. => 1. / 2.,
+        x if x < 0. => (1. - -x.sqrt().cosh()) / x,
         _ => panic!(),
     }
 }
 
 #[allow(unused)]
-pub fn stumpff_s(z: f64) -> f64 {
-    match z {
-        z if z > 0. => (z.sqrt() - z.sqrt().sin()) / z.powi(3).sqrt(),
-        z if z == 0. => 1. / 6.,
-        z if z < 0. => (-z.sqrt().sinh() - -z.sqrt()) / -z.powi(3).sqrt(),
+pub fn stumpff_s(x: f64) -> f64 {
+    match x {
+        x if x > 0. => (x.sqrt() - x.sqrt().sin()) / x.powi(3).sqrt(),
+        x if x == 0. => 1. / 6.,
+        x if x < 0. => (-x.sqrt().sinh() - -x.sqrt()) / -x.powi(3).sqrt(),
         _ => panic!(),
     }
 }
